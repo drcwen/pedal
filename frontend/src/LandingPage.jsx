4819561046e59b128f527affdd60e4006dccb58d
Wendel Derraco <wendelderraco@gmail.com>
2026-03-19 15:17:56 +0800

    first commit

---
+++ frontend/src/LandingPage.jsx
@@ -0,0 +1,43 @@
+import Navigation from './Navigation'
+
+function LandingPage() {
+
+  return (
+    <>
+      <div className='w-full'>
+
+        <Navigation />
+        
+        {/*Hero*/}
+        <div className='min-w-full min-h-screen bg-[url(./images/mobile_bg.png)] md:bg-[url(./images/hero_background.png)] bg-center bg-cover bg-no-repeat py-25 px-30 flex flex-col'>
+        
+          {/*PC Contents*/}
+          <div className='hidden md:flex flex-1 pt-20 flex flex-col justify-between'>
+            <div className='w-90 flex flex-col gap-3'>
+              <h1 className='text-7xl font-black text-yellow font-akagi text-shadow-lg'>Experience our family bikes!</h1>
+              <h1 className='text-3xl font-black text-white font-akagi text-shadow-lg'>at La Mesa Eco Park!</h1>
+            </div>
+
+            <div className='flex flex-row gap-5 items-center'>
+              <div className='bg-yellow border border-yellow border-3 px-10 py-3 rounded-sm text-center shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer'>
+                <h1 className='text-xl font-akagi font-black text-darkblue'>Rent Now!</h1>
+              </div>
+
+              <div className='border border-yellow border-3 px-10 py-3 rounded-sm text-center shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer'>
+                <h1 className='text-xl font-akagi font-black text-yellow'>Reserve</h1>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+
+        <div className='min-w-full min-h-screen bg-[#F7F7F7]'></div>
+        <div className='min-w-full min-h-screen bg-blue-800'></div>
+        
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
---
+++ frontend/src/Navigation.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import { IoMdCart } from "react-icons/io";
+import { FaUserAlt } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+
+function Navigation() {
+  const [scrolled, setScrolled] = useState(false);
+
+  // Scroll listener
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const navDesign =
+    'font-akagi text-lg font-bold text-white text-shadow-lg hover:text-yellow transition-all duration-300 hover:scale-105 cursor-pointer';
+
+  return (
+    <>
+    <div
+      className={`w-full fixed top-0 left-0 z-50 transition-colors duration-300 ${
+        scrolled ? 'bg-darkblue' : 'bg-transparent'
+      }`}
+    >
+      <div className='md:px-30 md:py-8 px-10 py-8 flex justify-between flex-row items-center'>
+        <div className='md:flex hidden'>
+          <h1 className='font-akagi text-2xl cursor-pointer font-black tracking-wide text-white text-shadow-lg hover:text-yellow transition-all duration-300 hover:scale-110'>
+            3JRemy
+          </h1>
+        </div>
+
+        <div className='md:hidden flex'>
+          <IoMenu className='text-white text-3xl'/>
+        </div>
+
+        <ul className='hidden md:flex flex flex-row gap-15'>
+          <li className={navDesign}>Home</li>
+          <li className={navDesign}>Rents</li>
+          <li className={navDesign}>Reservations</li>
+          <li className={navDesign}>Transactions</li>
+        </ul>
+
+        <div className='flex flex-row gap-8'>
+          <IoMdCart className='text-white text-xl text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110' />
+          <FaUserAlt className='text-white text-lg text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110'/>
+        </div>
+      </div>
+
+      <div
+        className={`w-full h-2 transition-all duration-300 
+          ${scrolled ? 'bg-blue opacity-100' : 'bg-transparent opacity-0'}`}
+      ></div>
+
+    </div>
+    
+
+    </>
+    
+  );
+}
+
+export default Navigation;
