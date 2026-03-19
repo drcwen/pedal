731e3fd8bdb74c05d684c18771f4d47c6d78fadf
Wendel Derraco <wendelderraco@gmail.com>
2026-03-19 16:17:27 +0800

    Fixed responsiveness in mobile navigation

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -9,7 +9,7 @@ function LandingPage() {
         <Navigation />
         
         {/*Hero*/}
-        <div className='min-w-full min-h-screen bg-[url(./images/mobile_bg.png)] md:bg-[url(./images/hero_background.png)] bg-center bg-cover bg-no-repeat py-25 px-30 flex flex-col'>
+        <div className='min-w-full min-h-screen md:py-25 md:px-30 py-27 px-10 bg-[url(./images/mobile_bg.png)] md:bg-[url(./images/hero_background.png)] bg-center bg-cover bg-no-repeat md:py-25 md:px-30 flex flex-col'>
         
           {/*PC Contents*/}
           <div className='hidden md:flex flex-1 pt-20 flex flex-col justify-between'>
@@ -29,7 +29,26 @@ function LandingPage() {
             </div>
           </div>
 
-          
+          {/*Mobile Contents*/}
+          <div className='md:hidden flex flex-1 flex-col pt-10 justify-between'>
+
+
+              <div className='w-60 flex flex-col gap-3'>
+                <h1 className='text-5xl font-akagi font-black text-yellow text-shadow-lg'>Experience our family bikes!</h1>
+                <h1 className='text-xl font-akagi font-black text-white text-shadow-lg'>at La Mesa Eco Park!</h1>
+              </div>
+
+              <div className='flex flex-row gap-4'>
+                <div className='px-5 py-2 bg-yellow border border-3 border-yellow w-fit rounded-md'>
+                  <h1 className='text-md font-akagi font-black text-darkblue'>Rent Now!</h1>
+                </div>
+
+                <div className='px-5 py-2 border border-3 border-yellow w-fit rounded-md'>
+                  <h1 className='text-md font-akagi font-black text-yellow'>Reserve</h1>
+                </div>
+              </div>
+
+          </div>
         </div>
 
         <div className='min-w-full min-h-screen bg-[#F7F7F7]'></div>
--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -2,9 +2,14 @@ import { useState, useEffect } from 'react';
 import { IoMdCart } from "react-icons/io";
 import { FaUserAlt } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
+import NavMobile from './NavMobile'
+import { IoClose } from "react-icons/io5";
 
 function Navigation() {
   const [scrolled, setScrolled] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  
 
   // Scroll listener
   useEffect(() => {
@@ -27,43 +32,55 @@ function Navigation() {
 
   return (
     <>
-    <div
-      className={`w-full fixed top-0 left-0 z-50 transition-colors duration-300 ${
-        scrolled ? 'bg-darkblue' : 'bg-transparent'
-      }`}
-    >
-      <div className='md:px-30 md:py-8 px-10 py-8 flex justify-between flex-row items-center'>
-        <div className='md:flex hidden'>
-          <h1 className='font-akagi text-2xl cursor-pointer font-black tracking-wide text-white text-shadow-lg hover:text-yellow transition-all duration-300 hover:scale-110'>
-            3JRemy
-          </h1>
+
+      <div
+        className={`w-full fixed top-0 left-0 z-50 transition-colors duration-300 ${
+          scrolled ? 'bg-darkblue' : 'bg-transparent'
+        }`}
+      >
+        <div className='md:px-30 md:py-8 px-10 py-8 flex justify-between flex-row items-center'>
+          <div className='md:flex hidden'>
+            <h1 className='font-akagi text-2xl cursor-pointer font-black tracking-wide text-white text-shadow-lg hover:text-yellow transition-all duration-300 hover:scale-110'>
+              3JRemy
+            </h1>
+          </div>
+
+          {/*Mobile Menu Button*/}
+            <div className='md:hidden flex'>
+              <button onClick={() => setMenu(prev => !prev)}
+                className='text-3xl text-white'>
+                  {menu ? <IoClose /> : <IoMenu />}
+                </button>
+            </div>
+
+          <ul className='hidden md:flex flex flex-row gap-15'>
+            <li className={navDesign}>Home</li>
+            <li className={navDesign}>Rents</li>
+            <li className={navDesign}>Reservations</li>
+            <li className={navDesign}>Transactions</li>
+          </ul>
+
+          <div className='flex flex-row gap-8'>
+            <IoMdCart className='text-white text-xl text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110' />
+            <FaUserAlt className='text-white text-lg text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110'/>
+          </div>
         </div>
 
-        <div className='md:hidden flex'>
-          <IoMenu className='text-white text-3xl'/>
-        </div>
+        <div
+          className={`w-full h-2 transition-all duration-300 
+            ${scrolled ? 'bg-blue opacity-100' : 'bg-transparent opacity-0'}`}
+        ></div>
 
-        <ul className='hidden md:flex flex flex-row gap-15'>
-          <li className={navDesign}>Home</li>
-          <li className={navDesign}>Rents</li>
-          <li className={navDesign}>Reservations</li>
-          <li className={navDesign}>Transactions</li>
-        </ul>
+        
 
-        <div className='flex flex-row gap-8'>
-          <IoMdCart className='text-white text-xl text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110' />
-          <FaUserAlt className='text-white text-lg text-shadow-lg hover:text-yellow transition-all cursor-pointer duration-300 hover:scale-110'/>
-        </div>
       </div>
 
       <div
-        className={`w-full h-2 transition-all duration-300 
-          ${scrolled ? 'bg-blue opacity-100' : 'bg-transparent opacity-0'}`}
-      ></div>
-
-    </div>
-    
-
+        className={`fixed top-24 left-0 w-full transform transition-transform duration-300
+          ${menu ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}`}
+      >
+        <NavMobile />
+      </div>
     </>
     
   );
